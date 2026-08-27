975fde0ae4321282695f1a5331b5d84e658e0934
PTKA <phuree.teekapak@gmail.com>
2026-08-27 23:23:48 +0700

    test

--- sort_func/sort.h
+++ sort_func/sort.h
@@ -1,5 +1,16 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include "bubble-sort.c"
+#include "insertion-sort.c"
+#include "merge.c"
+#include "quick.c"
+#include "selection-sort.c"
+
 void merge_sort(int arr[], int lenght);
 void quick_sort(int arr[], int lenght);
 void SelectionSort(int arr[], int n);
 void InsertionSort(int a[], int n);
 void BubbleSort(int a[], int n);
+
+#endif
