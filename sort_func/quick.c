7a365fe234a4e5f673b7de4d2d6d1c59bded24b9
PTKA <phuree.teekapak@gmail.com>
2026-09-09 17:04:08 +0700

    Update quick.c

--- sort_func/quick.c
+++ sort_func/quick.c
@@ -76,7 +76,6 @@ void quick_recursive_randomP(int arr[], int left, int right)
 {
     if (left < right)
     {
-        srand(time(NULL));
         // ----------------------------------------------------
         // ส่วนที่เพิ่มเข้ามา: สุ่ม Pivot
         // สุ่ม index ให้อยู่ในช่วง [left, right]
